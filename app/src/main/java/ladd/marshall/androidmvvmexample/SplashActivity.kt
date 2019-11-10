34a6c472681e0711578706e6a832f91478cef4ac
sortagreg <ladd.marshall@gmail.com>
2019-11-09 20:14:03 -0600

    add activities

---
+++ app/src/main/java/ladd/marshall/androidmvvmexample/SplashActivity.kt
@@ -0,0 +1,17 @@
+package ladd.marshall.androidmvvmexample
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+        finish()
+    }
+}
