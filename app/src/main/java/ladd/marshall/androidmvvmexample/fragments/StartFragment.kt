8c93a412c147e91757c466a6e12d4a55c448e8fb
Marshall Ladd <ladd.marshall@gmail.com>
2019-11-14 12:39:26 -0600

    Remove unneeded ViewModel

--- app/src/main/java/ladd/marshall/androidmvvmexample/fragments/StartFragment.kt
+++ app/src/main/java/ladd/marshall/androidmvvmexample/fragments/StartFragment.kt
@@ -3,14 +3,11 @@ package ladd.marshall.androidmvvmexample.fragments
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
 import kotlinx.android.synthetic.main.start_fragment.*
 import ladd.marshall.androidmvvmexample.R
 
 class StartFragment : Fragment(R.layout.start_fragment) {
-
-    private val viewModel by viewModels<StartViewModel>()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
