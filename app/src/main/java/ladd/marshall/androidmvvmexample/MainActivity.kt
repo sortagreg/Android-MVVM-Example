b0f2c11fb4230c0d8aeb4a76d62148576aa9710b
Marshall Ladd <ladd.marshall@gmail.com>
2019-11-13 21:06:21 -0600

    Set up navigation graph
    CHECKPOINT.  Ensure the app runs and you see 'start_fragment' in the toolbar and 'Hello' on the screen in the top left corner.  If this is not the case, troubleshoot until it is.

--- app/src/main/java/ladd/marshall/androidmvvmexample/MainActivity.kt
+++ app/src/main/java/ladd/marshall/androidmvvmexample/MainActivity.kt
@@ -2,11 +2,22 @@ package ladd.marshall.androidmvvmexample
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.NavigationUI
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        navController = findNavController(R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
